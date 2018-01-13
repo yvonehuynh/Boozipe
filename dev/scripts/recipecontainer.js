74379226091adc32a2794bf837e9d30845654c8b
Yvone Huynh <sw.yvone.huynh@gmail.com>
2018-01-13 11:28:29 -0500

    converted array into unordered list

--- dev/scripts/recipecontainer.js
+++ dev/scripts/recipecontainer.js
@@ -3,7 +3,8 @@ import React from 'react';
 export default class Recipecontainer extends React.Component {
     render(){
         const recipe = this.props.recipe;
-        const recipeIngredients = recipe.ingredients.toString();
+        let recipeIngredients = recipe.ingredients;
+        recipeIngredients = recipeIngredients.map((item, index) => <li key={index}>{item}</li>);
         return (
             <div>
                 <li>{recipe.recipeName}</li>
